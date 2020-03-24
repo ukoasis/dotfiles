8ef346e8ab1e7412a6e550e61b4ca9f70cf4fa14
ukoasis <loveoasisforever@gmail.com>
2020-03-24 16:46:02 +0900

    Fix handling that checkout a remote branch

--- dotfish/functions/peco_checkout_git_branch.fish
+++ dotfish/functions/peco_checkout_git_branch.fish
@@ -3,12 +3,15 @@ function peco_checkout_git_branch
     git branch -a | peco | tr -d ' ' | read branch
     echo $branch
     if [ $branch ]
-        if contains $branch "remotes/"
-            set -l b (echo $branch | awk -F'/' '{print $3}')
-            git checkout -b $b $branch
-        else
-            git checkout $branch
-        end
+      set -l is_remote (string match "remotes\/*" $branch)
+      echo $is_remote
+      if [ -n is_remote ]
+        set -l b (string replace "remotes/origin/" "" $branch)
+        echo $b
+        git checkout $b
+      else
+        git checkout $branch
+      end
     end
     commandline -f repaint
   else
