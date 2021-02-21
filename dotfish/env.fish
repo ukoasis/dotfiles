5d7fe02e5932d8ae47c7d137e47835a0b36cdf62
ukoasis <loveoasisforever@gmail.com>
2021-02-22 00:36:29 +0900

    Add path for arm mac

--- dotfish/env.fish
+++ dotfish/env.fish
@@ -7,21 +7,25 @@ set -x LESSCHARSET utf-8
 set -x EDITOR nvim
 
 # Path
-set -U fish_user_paths $HOME/.asdf/shims
-set -U fish_user_paths $HOME/dotfiles/bin
-set -U fish_user_paths $HOME/.bin
-set -U fish_user_paths $HOME/.local/bin
-set -U fish_user_paths ./bin
-set -U fish_user_paths $HOME/.ghq/github.com/apple/sourcekit-lsp/.build/release
-set -U fish_user_paths (yarn global bin)
-set -U fish_user_paths /usr/local/bin
-set -U fish_user_paths /usr/local/sbin
-set -U fish_user_paths /sbin
+set -x PATH /usr/local/bin $PATH
+set -x PATH $HOME/.asdf/shims $PATH
+set -x PATH $HOME/dotfiles/bin $PATH
+set -x PATH $HOME/.bin $PATH
+set -x PATH $HOME/.local/bin $PATH
+set -x PATH ./bin $PATH
+set -x PATH $HOME/ghq/github.com/apple/sourcekit-lsp/.build/releasea $PATH
+if test (uname -m) = "arm64"
+  set -x PATH /opt/homebrew/bin $PATH
+end
+set -x PATH /usr/local/bin $PATH
+set -x PATH (yarn global bin) $PATH
+set -x PATH /usr/local/sbin $PATH
+set -x PATH /sbin $PATH
 set -x MANPATH $MANPATH /opt/local/man /usr/local/share/man
 
 # homebrew
 set -x HOMEBREW_NO_ANALYTICS 1
-set -x HOMEBREW_GITHUB_API_TOKEN (security find-generic-password -a 'homebrew-github-token' -w)
+set -x HOMEBREW_GITHUB_API_TOKEN (security find-generic-password -a "$USER" -s 'homebrew-github-token' -w)
 
 # cocoapods
 set -x COCOAPODS_DISABLE_STATS true
